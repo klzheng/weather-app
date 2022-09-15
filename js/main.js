643b734236d2abedf0a4c4b6bad5b84eba01ffc2
Kevin <klzheng@buffalo.edu>
2022-09-14 22:59:21 -0400

    refactored images to change depending on weather

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const query = "?zip=11201,us&appid="
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather" 
 const API_URL = BASE_URL + query + API_KEY
 
+
 async function getWeatherData() {
     const res = await fetch(API_URL)
     const data = await res.json()
@@ -13,15 +14,14 @@ async function getWeatherData() {
     const windSpeed = data.wind.speed
     const date = new Date(data.dt * 1000)
     const temperature = tempToImperial(data.main.temp)
-    const tempFeel = data.main.feels_like
+    // const tempFeel = data.main.feels_like
     const tempRange = [tempToImperial(data.main.temp_max), tempToImperial(data.main.temp_min)]
     const humidity = data.main.humidity
-    const rain = getRain(data.rain)
-
-    console.log(data)
-
-    const [icon, condition] = weatherIdToIcon(weatherId)
+    const rain = getRain(data.rain) 
+    const [icon, condition, folderPath] = weatherIdToIcon(weatherId)
+    const imgURL = getRandomImg(folderPath)
     const weatherInfo = document.createElement("div")
+
     weatherInfo.innerHTML = 
     `
     <section class="weather-summary">
@@ -48,8 +48,13 @@ async function getWeatherData() {
         </ul>
     </section>
     `
+    console.log(imgURL)
+    document.querySelector("body").style.background = `url(${imgURL})`
+    document.querySelector("body").style["background-size"] = "cover"
     document.querySelector("body").appendChild(weatherInfo)
+    console.log(data)
 }
+
 
 function getCountry(abbr) {
     const regionNames = new Intl.DisplayNames(
@@ -58,30 +63,60 @@ function getCountry(abbr) {
     return regionNames.of(abbr)
 }
 
+
 function getRain(data) {
     return data === undefined ? 0 : (data/25.4).toPrecision(2)
 }
+
 
 function tempToImperial(temp) {
     return Math.round((temp - 273.15) * (9/5) + 32)
 }
 
+
 function weatherIdToIcon(id) {
     if (id < 300) {
-        return [`<i class="fa-solid fa-cloud-showers-heavy"></i>, "Thunderstorm`]
+        return [`<i class="fa-solid fa-cloud-showers-heavy"></i>`, "Thunderstorm", "rain"]
     } else if (id < 400) {
-        return `<i class="fa-solid fa-droplet"></i>`
+        return [`<i class="fa-solid fa-droplet"></i>`, "Drizzling", "rain/"]
     } else if (id < 600) {
-        return `<i class="fa-solid fa-cloud-rain"></i>`
+        return [`<i class="fa-solid fa-cloud-rain"></i>`,"Raining", "rain/"]
     } else if (id < 700) {
-        return `<i class="fa-regular fa-snowflake"></i>`
+        return [`<i class="fa-regular fa-snowflake"></i>`,"Snowing", "snow/"]
     } else if (id < 800) {
-        return `<i class="fa-solid fa-smog"></i>`
+        return [`<i class="fa-solid fa-smog"></i>`,"Fog", "atmosphere/"]
     } else if (id === 800) {
-        return [`<i class="fa-solid fa-sun"></i>`, "Clear"]
+        return [`<i class="fa-solid fa-sun"></i>`, "Clear", "clear-day/"]
     } else {
-        return `<i class="fa-solid fa-cloud"></i>`
+        return [`<i class="fa-solid fa-cloud"></i>`,"Cloudy", "cloudy/"]
     }
+}
+
+
+function getRandomImg(path) {
+    const root = "./images/"
+    let index = 0
+    const images = ["bg68.jpg", "bg63.jpg", "bg57.jpg", "bg69.jpg", "background3.jpg", "bg11.jpg", "bg48.jpg"]
+
+    switch (path) {
+        case "cloudy/": 
+            index = 0;
+            break
+        case "rain/":
+            index = 1;
+            break
+        case "snow/":
+            index = 3;
+            break
+        case "atmosphere/":
+            index = 4;
+            break
+        case "clear-day/":
+            index = 5;
+            break
+    }
+
+    return `${root + path + images[index]}`
 }
 
 getWeatherData()
